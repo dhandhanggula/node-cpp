3ce9befc2bfe3e2655df0bddc775640f1d738908
Fandi Adinata <09.fandisurya@gmail.com>
2022-06-15 10:51:32 +0700

    fix reverse route path

--- src/main/answer.ino
+++ src/main/answer.ino
@@ -11,29 +11,26 @@ void answer(String message)
 {
   String getMsgCode = parsing(message, '|', 0);
   String getMsgID = parsing(message, '|', 1);
-  String msgSender = parsing(message, '|', 2);
-  String msgRecipient = parsing(message, '|', 3);
-  String msgPath = parsing(message, '|', 4);
-  String msgPayload = parsing(message, '|', 5);
+  String getMsgSender = parsing(message, '|', 2);
+  String getMsgRecipient = parsing(message, '|', 3);
+  String getMsgPath = parsing(message, '|', 4);
+  String getMsgPayload = parsing(message, '|', 5);
 
   String sendPath;
 
   //Save new path
-  int pathLength = charMode(msgPath, ',');
+  int pathLength = charMode(getMsgPath, ',');
   String path[] = {};
 
-  if(pathLength == 0)
-  {
-    sendPath = msgSender;
-  }
-
+  if(pathLength == 0){sendPath = getMsgSender;}
   if(pathLength != 0)
   {
     for(int i=0; i <= pathLength; i++)
     {
-      path[i] = parsing(msgPath, ',', i);
+      path[pathLength - i] = parsing(getMsgPath, ',', i);
     }
 
+    // reverse route path back to sender
     for(int i=0; i<= pathLength; i++)
     {
       if(path[i] != "")
@@ -44,24 +41,25 @@ void answer(String message)
   }
 
 
-  int sendPathLength = sendPath.length() - 1;
-  String lastChar = String(sendPath.charAt(sendPathLength));
-
+  int sendPathLength = sendPath.length();
+  String lastChar = String(sendPath.charAt(sendPathLength - 1));
   if(lastChar == ",")
   {
-    sendPath.remove(sendPathLength);
+    sendPath.remove(sendPathLength - 1);
   }
 
-  Serial.println(sendPath);
+  //Serial.println(sendPath);
   
+
+  // Answer Ping MSG
   if(getMsgCode == code("ping"))
   {
-    if(msgRecipient == nodeID)
+    if(getMsgRecipient == nodeID)
     {
       // send answer
       String msgCode = code("ansPing");
       String msgID = getMsgID;
-      String destination = msgSender;
+      String destination = getMsgSender;
       String path = sendPath;
       String sentMsg = msgCode + parser + msgID + parser + nodeID + parser + destination + parser + path + parser + "99";
 

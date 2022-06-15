b2e9e317ee826d2ba25e28aeaa9caf2b380a79a2
Fandi Adinata <09.fandisurya@gmail.com>
2022-06-15 22:34:36 +0700

    rreq

--- src/main/code.ino
+++ src/main/code.ino
@@ -2,13 +2,29 @@
 //====================================================================
 // Message code
 // List :
+//
+// General Purpose
 // - 00   ==> ping
-// - 80   ==> answer ping
+// - 01   ==> answer Ping
+//
+// Messaging
+// - 10   ==> send msg
+//
+// DSR Routing
+// - 20   ==> Route request (RREQ)
+// - 21   ==> Route reply (RREP)
+//
+// Network Maintenance
+// - 30   ==> Route maintenance(RERR)
 //====================================================================
 //====================================================================
 
 String code(String codeName)
 {
   if(codeName == "ping"){return "00";}
-  if(codeName == "ansPing"){return "80";}
+  if(codeName == "ansPing"){return "01";}
+  if(codeName == "msg"){return "10";}
+  if(codeName == "rreq"){return "20";}
+  if(codeName == "rrep"){return " 21";}
+  if(codeName == "rerr"){return "30";}
 }
